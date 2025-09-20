926747f1db5cb1a8acb720a936d6753738bbec2d
Yurashk <yurahi@ukr.net>
2025-09-21 02:17:11 +0300

    deploy: Angular build to root (GitHub Pages)

--- src/app/services/cpas.ts
+++ src/app/services/cpas.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { DOCUMENT, inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CpasCenter } from '../models/cpas-center';
 
@@ -7,22 +7,12 @@ import { CpasCenter } from '../models/cpas-center';
   providedIn: 'root'
 })
 export class Cpas {
-  private doc = inject(DOCUMENT);
   constructor(private http: HttpClient) {}
 
-  private asset(relPath: string): string {
-    return new URL(relPath, this.doc.baseURI).toString();
+  getCenters(currentCity:string): Observable<CpasCenter[]> {
+    return this.http.get<CpasCenter[]>(`/data/cpas-${currentCity}.mock.json`);
   }
-
-  getCenters(currentCity: string): Observable<CpasCenter[]> {
-    return this.http.get<CpasCenter[]>(
-      this.asset(`data/cpas-${currentCity}.json`)
-    );
-  }
-
   getCentersByStreet(): Observable<any[]> {
-    return this.http.get<any[]>(
-      this.asset('data/postal_code_to_cpas_id.json')
-    );
+    return this.http.get<any[]>('/data/postal_code_to_cpas_id.json');
   }
 }
